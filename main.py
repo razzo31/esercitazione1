d2522abcfe56011ddfdab89e7bea61a440403013
Federico <razzo3125@gmail.com>
2020-03-23 13:20:48 +0100

    modifica main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ print("ciao", nome)
 age= int(input("quanti anni hai? "))
 n = int(input("inserisci un numero: "))
 print(n*2) 
+
+#########
+if n < 5:
+print("ciaomare")
+elif n > 6:
+print("bellachicco")
+else:
+print("perdente")
